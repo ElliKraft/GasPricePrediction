ec1c95e8255dab62c24a5cfb28c1371334688bae
Cassandra <cassandra.schilling00@gmail.com>
2021-12-05 22:42:05 +0100

    Created prediction model

--- Main.py
+++ Main.py
@@ -4,90 +4,62 @@ import numpy
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import statsmodels.graphics.api as smg
 from scipy import interpolate
 from scipy.interpolate import UnivariateSpline
 import matplotlib.patches as mpatches
-
-from statsmodels.tsa.ar_model import AutoReg
-
 
 originalData = pd.read_csv('GasPrice.csv',
                             index_col=['Halbjahr'],
                             parse_dates=['Halbjahr'],
                             sep=r'\s*;\s*')
-# print(originialData)
-
-
-#Stack Overflow
-# array = np.random.random_integers(0, 10, (10, 10)).astype(float)
-#values greater then 7 goes to np.nan
-# array[array > 7] = np.nan
-# print(array)
-#
-# x = np.arange(0, data.shape[1]) #Spalten
-# y = np.arange(0, data.shape[0]) #Zeilen
-# #mask invalid values
-# array = np.ma.masked_invalid(data)
-# xx, yy = np.meshgrid(x, y)
-# #get only the valid values
-# x1 = xx[~array.mask]
-# y1 = yy[~array.mask]
-# newarr = array[~array.mask]
-#
-# GD1 = interpolate.griddata((x1, y1), newarr.ravel(),
-#                            (xx, yy),
-#                            method='cubic')
-
-# print(GD1)
-# print(array[~array.mask])
-# print(np.meshgrid(x, y))
-
-# x1 = data["Netzentgelt,  ct/KWh"]
-# y1 = data["Halbjahr"]
-
-# x = np.arange(0, 14)
-# y = originialData['Netzentgelt,  ct/KWh']
-# f = interpolate.interp1d(x, y)
-# print(f)
-
-#
-# s = UnivariateSpline(x, y, k=1)
-# y=s(x)
-
-# fit = np.polyfit(x, data['Netzentgelt,  ct/KWh'], 1)
-# fit = interpolate.interp1d(x, y, fill_value="extrapolate")
-# print(fit(10))
-
-# print(data["Netzentgelt,  ct/KWh"])
-
 
 cleanedData = originalData
+cleanedData.drop(cleanedData.tail(4).index, inplace=False)
 
+predictionData = originalData
+print(predictionData)
+# s = pd.Series(cleanedData["Netzentgelt,  ct/KWh"])
+t = pd.Series(predictionData["Vertrieb und Marge, ct/KWh"])
+w = pd.Series(predictionData["Energiebeschaffung, ct/KWh"])
 
-s = pd.Series(cleanedData["Netzentgelt,  ct/KWh"])
-t = pd.Series(cleanedData["Vertrieb und Marge, ct/KWh"])
 u = pd.Series(cleanedData["Endverbraucherpreis,  ct/KWh"])
-
-cleanedData["Netzentgelt,  ct/KWh"] = s.interpolate()
-cleanedData["Vertrieb und Marge, ct/KWh"] = t.interpolate(method='cubic', order=3)
-cleanedData["Endverbraucherpreis,  ct/KWh"] = u.interpolate()
-print("cleanedData")
-print(cleanedData)
-
 #
+# cleanedData["Netzentgelt,  ct/KWh"] = s.interpolate()
+predictionData["Vertrieb und Marge, ct/KWh"] = t.interpolate()
+predictionData["Energiebeschaffung, ct/KWh"] = w.interpolate()
+predictionData["Endverbraucherpreis,  ct/KWh"] = u.interpolate()
+print("predictionData")
+print(predictionData)
 
-predictionData = cleanedData[["Energiebeschaffung, ct/KWh", "Endverbraucherpreis,  ct/KWh"]]
-# print(predictionData)
 
+X = cleanedData[["Energiebeschaffung, ct/KWh", "Vertrieb und Marge, ct/KWh"]]
+# X = cleanedData["Energiebeschaffung, ct/KWh"]
+# print(X)
+y = cleanedData["Endverbraucherpreis,  ct/KWh"]
+Z = predictionData[["Energiebeschaffung, ct/KWh", "Vertrieb und Marge, ct/KWh"]]
+# fit a OLS model with intercept on Energiebeschaffung and Marge und Vertrieb
+X = sm.add_constant(X)
+Z = sm.add_constant(Z)
+model = sm.OLS(y, X).fit()
+prediction = model.predict(Z)
+model.summary()
+print(model.summary())
+print(prediction)
 
+# corr_matrix = np.corrcoef(cleanedData[["Energiebeschaffung, ct/KWh", "Endverbraucherpreis,  ct/KWh"]])
+# smg.plot_corr(corr_matrix, xnames=cleanedData["Endverbraucherpreis,  ct/KWh"], ynames=cleanedData["Energiebeschaffung, ct/KWh"])
+# plt.show()
+# print(corr_matrix)
 
-plt.plot(cleanedData.index, cleanedData["Energiebeschaffung, ct/KWh"], label='Energiebeschaffung')
-plt.plot(cleanedData.index, cleanedData["Steuern und Abgaben, ct/KWh"], label='Steuern und Abgaben')
-plt.plot(cleanedData.index, cleanedData["Netzentgelt,  ct/KWh"], label='Netzentgelt')
-plt.plot(cleanedData.index, cleanedData["Vertrieb und Marge, ct/KWh"], label='Vertrieb und Marge')
-plt.plot(cleanedData.index, cleanedData["Endverbraucherpreis,  ct/KWh"], label='Endverbraucherpreis')
-plt.subplots_adjust(right=0.77)
-plt.title('Halbjährliche Daten zu Gaspreisen in Deutschland')
-plt.grid(True)
-plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
-plt.show()
+# plt.plot(cleanedData.index, cleanedData["Energiebeschaffung, ct/KWh"], label='Energiebeschaffung')
+# plt.plot(cleanedData.index, cleanedData["Steuern und Abgaben, ct/KWh"], label='Steuern und Abgaben')
+# plt.plot(cleanedData.index, cleanedData["Netzentgelt,  ct/KWh"], label='Netzentgelt')
+# plt.plot(cleanedData.index, cleanedData["Vertrieb und Marge, ct/KWh"], label='Vertrieb und Marge')
+# plt.plot(cleanedData.index, cleanedData["Endverbraucherpreis,  ct/KWh"], label='Endverbraucherpreis')
+# plt.subplots_adjust(right=0.77)
+# plt.title('Halbjährliche Daten zu Gaspreisen in Deutschland')
+# plt.grid(True)
+# plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+# plt.show()
